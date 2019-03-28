a018a907fb73fb3ce42db06e6631d75615c3cc71
Pavel Sergeev <pavel.sergeev@jetbrains.com>
2019-03-28 15:16:53 +0300

    read port file

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod text;
 mod dom;
 
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, BufReader};
 
 use glutin::{Event, ElementState, MouseButton};
 use glutin::EventsLoop;
@@ -19,7 +19,8 @@ use webrender::Renderer;
 use text::*;
 use std::time::{SystemTime, Instant};
 use std::env;
-use std::net::ToSocketAddrs;
+use std::net::{ToSocketAddrs, Ipv4Addr};
+use serde::Deserialize;
 
 fn create_window(events_loop: &EventsLoop) -> GlWindow {
     let window_builder = glutin::WindowBuilder::new()
@@ -259,12 +260,22 @@ fn get_text(file_path: String) -> String {
     return content
 }
 
+#[derive(Deserialize)]
+struct PortFileContent {
+    #[serde(rename = "httpPort")]
+    http_port: u16,
+    #[serde(rename = "tcpPort")]
+    tcp_port: u16
+}
+
 fn main() -> std::io::Result<()> {
     env_logger::init();
     let args: Vec<String> = env::args().collect();
     if args.len() >= 2 {
-        let addr = &args[1];
-        run_event_loop(addr);
+        let port_file = &args[1];
+        let f = File::open(port_file).expect("No port file");
+        let content: PortFileContent = serde_json::from_reader(BufReader::new(f)).unwrap();
+        run_event_loop((Ipv4Addr::new(127, 0, 0, 1), content.tcp_port));
     }
     return Ok(());
 }
